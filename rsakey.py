d87b8f29b6e2549dc9d0e8821c5841c2fe78c372
elisha johnsunder <ejohn3@umbc.edu>
2017-04-07 18:10:23 -0400

    Update rsakey.py

--- rsakey.py
+++ rsakey.py
@@ -5,4 +5,9 @@ from time import sleep
 token = ""
 manager = digitalocean.Manager(token=token)
 keys = manager.get_all_sshkeys()
-print(keys)
+print("SSH keys:")
+print([key.id for key in keys])
+images = manager.get_my_images()
+print("Image IDs:")
+print([image.id for image in images])
+
